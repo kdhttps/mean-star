28e28ad37bd438a347f7ea00cd41a66792f1fc3b
kdhttps <kdhttps@gmail.com>
2020-05-08 16:07:34 +0530

    feat: Added ng-bootstrap and toast message

--- client/src/app/shared/index.ts
+++ client/src/app/shared/index.ts
@@ -1,3 +1,4 @@
 export * from './layout';
 export * from './show-authed.directive';
 export * from './shared.module';
+export * from './toast-service';
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,11 +3,31 @@ import { ShowAuthedDirective } from './show-authed.directive';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastsContainer } from './toasts-container.component';
+import { ToastService } from './toast-service';
 
 @NgModule({
-  imports: [HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [ShowAuthedDirective],
-  exports: [ShowAuthedDirective, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  declarations: [
+    ShowAuthedDirective,
+    ToastsContainer,
+  ],
+  exports: [
+    ShowAuthedDirective,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    ToastService,
+  ],
 })
 export class SharedModule {
 
